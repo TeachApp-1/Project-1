a9a2b160481643288add537b52d8802bf4f77df9
YardenHayut <yardenhayut1@gmail.com>
2020-11-30 23:19:06 +0200

    yarden

---
+++ Teacher.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Teacher extends User {
+    Scanner in = new Scanner(System.in);
+
+    public Teacher(String uName, String uEmail, String uPassword) {
+        super(uName, uEmail, uPassword);
+    }
+
+    @Override
+    public String toString() {
+        return "\n(Teacher)" +
+                "\nUser: #" + getId() +
+                "\nuser Name:'" + userName + '\'' +
+                "\nuser Email: '" + userEmail + '\'' +
+                "\nuser Password: '" + userPassword + '\'' +
+                "\n";
+    }
+
+    public void wrongUserInput(){
+        System.out.println("Your user not valid");
+    }
+
+    public void wrongUserInput(String uName){
+        System.out.println("Your user name/ password not valid");
+    }
+
+    @Override
+    public boolean login() {
+        System.out.println("Enter user name:");
+        String uName = in.nextLine();
+        System.out.println("Enter password:");
+        String uPassword = in.nextLine();
+        if(uName.equals(userName) && uPassword.equals(userPassword))
+            return true;
+        else if(uPassword.equals(userPassword))
+            wrongUserInput();
+        else
+            wrongUserInput(uName);
+        return false;
+    }
+
+    public void editUser() {
+        System.out.println("Edit user name?");
+        System.out.println("Yes- Y/y");
+        String temp = in.nextLine();
+        if (temp.equals("Y") || temp.equals("y")) {
+            System.out.println("Set your new user name:");
+            temp = in.nextLine();
+            System.out.print("Your user name has been changed from: " + this.userName + " ");
+            this.setUserName(temp);
+            System.out.println("to -> " + temp);
+        }
+        System.out.println("Edit password?");
+        System.out.println("Yes- Y/y");
+        temp = in.nextLine();
+        if (temp.equals("y") || temp.equals("Y")) {
+            System.out.println("Set your new password:");
+            temp = in.nextLine();
+            System.out.print("Your password has been changed from: " + this.userPassword + " ");
+            this.setUserPassword(temp);
+            System.out.println("to -> " + temp);
+        }
+        System.out.println("Edit Email?");
+        System.out.println("Yes- Y/y");
+        temp = in.nextLine();
+        if (temp.equals("y") || temp.equals("Y")) {
+            System.out.println("Set your new Email:");
+            temp = in.nextLine();
+            System.out.print("Your Email has been changed from: " + this.userEmail + " ");
+            this.setUserEmail(temp);
+            System.out.println("to -> " + temp);
+        }
+        System.out.println("Your details now:");
+        System.out.println(this.toString());
+    }
+}
