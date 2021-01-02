07f1a6776fdf5b5abb136195eceb9746f564a58b
daniel752 <62557588+daniel752@users.noreply.github.com>
2021-01-02 21:44:44 +0200

    QuizAdmin
    complete

---
+++ QuizAdminApp/app/src/main/java/com/example/quizadmin/QuestionAdapter.java
@@ -0,0 +1,197 @@
+package com.example.quizadmin;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.ArrayMap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.example.quizadmin.CategoryActivity.catList;
+import static com.example.quizadmin.CategoryActivity.selectedCatIndex;
+import static com.example.quizadmin.SetsActivity.selectedSetIndex;
+import static com.example.quizadmin.SetsActivity.setIDs;
+
+public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHolder>
+{
+    private List<QuestionModel> questionList;
+
+    public QuestionAdapter(List<QuestionModel> questionList)
+    {
+        this.questionList = questionList;
+    }
+
+    @NonNull
+    @Override
+    public QuestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType)
+    {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.category_item_layout,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestionAdapter.ViewHolder viewHolder, int position)
+    {
+        viewHolder.setData(position,this);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return questionList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView title;
+        private ImageView deleteBtn;
+        private Dialog loadingDialog;
+
+        public ViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.catName);
+            deleteBtn = itemView.findViewById(R.id.catDeleteBtn);
+
+            loadingDialog = new Dialog(itemView.getContext());
+            loadingDialog.setContentView(R.layout.loading_progressbar);
+            loadingDialog.setCancelable(false);
+            loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progressbar_background);
+            loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+
+        private void setData(final int position,QuestionAdapter questionAdapter)
+        {
+            title.setText("QUESTION "+ String.valueOf(position + 1));
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Intent intent = new Intent(itemView.getContext(),QuestionDetailActivity.class);
+                    intent.putExtra("ACTION","EDIT");
+                    intent.putExtra("Q_ID",position);
+                    itemView.getContext().startActivity(intent);
+                }
+            });
+
+            deleteBtn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    AlertDialog alertDialog = new AlertDialog.Builder(itemView.getContext())
+                            .setTitle("Delete Question")
+                            .setMessage("Do you want to delete this question?")
+                            .setPositiveButton("Delete", new DialogInterface.OnClickListener()
+                            {
+                                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                                @Override
+                                public void onClick(DialogInterface dialog, int which)
+                                {
+                                    deleteQuestion(position,itemView.getContext(),questionAdapter);
+                                }
+                            }).setNegativeButton("Cancel",null)
+                            .setIcon(android.R.drawable.ic_dialog_alert).show();
+
+                    alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setBackgroundColor(Color.RED);
+                    alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setBackgroundColor(Color.RED);
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(0,0,50,0);
+                    alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setLayoutParams(params);
+                }
+            });
+        }
+
+        private void deleteQuestion(final int position, Context context,QuestionAdapter questionAdapter)
+        {
+            loadingDialog.show();
+
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                    .collection(setIDs.get(selectedSetIndex)).document(questionList.get(position).getQuestionID())
+                    .delete()
+                    .addOnSuccessListener(new OnSuccessListener<Void>()
+                    {
+                        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                        @Override
+                        public void onSuccess(Void aVoid)
+                        {
+                            Map<String,Object> questionDoc = new ArrayMap<>();
+                            int index = 1;
+
+                            for(int i=0;i<questionList.size();i++)
+                            {
+                                if(i != position)
+                                {
+                                    questionDoc.put("Q"+String.valueOf(index) + "_ID",questionList.get(i).getQuestionID());
+                                    index++;
+                                }
+                            }
+
+                            questionDoc.put("COUNT",String.valueOf(index-1));
+
+                            firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                                    .collection(setIDs.get(selectedSetIndex)).document("QUESTION_LIST")
+                                    .set(questionDoc)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>()
+                                    {
+                                        @Override
+                                        public void onSuccess(Void aVoid)
+                                        {
+                                            Toast.makeText(context,"Question deleted successfully",Toast.LENGTH_SHORT).show();
+
+                                            questionList.remove(position);
+                                            questionAdapter.notifyDataSetChanged();
+                                            loadingDialog.dismiss();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener()
+                            {
+                                @Override
+                                public void onFailure(@NonNull Exception e)
+                                {
+                                    Toast.makeText(context,e.getMessage(),Toast.LENGTH_SHORT).show();
+                                    loadingDialog.dismiss();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener()
+            {
+                @Override
+                public void onFailure(@NonNull Exception e)
+                {
+                    Toast.makeText(context,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    loadingDialog.dismiss();
+                }
+            });
+
+        }
+    }
+}
---
+++ QuizAdminApp/app/src/main/java/com/example/quizadmin/QuestionDetailActivity.java
@@ -0,0 +1,255 @@
+package com.example.quizadmin;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Dialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.ArrayMap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+import static com.example.quizadmin.CategoryActivity.catList;
+import static com.example.quizadmin.CategoryActivity.selectedCatIndex;
+import static com.example.quizadmin.QuestionActivity.questionList;
+import static com.example.quizadmin.SetsActivity.selectedSetIndex;
+import static com.example.quizadmin.SetsActivity.setIDs;
+
+public class QuestionDetailActivity extends AppCompatActivity
+{
+    private EditText question,optionA,optionB,optionC,optionD,answer;
+    private Button addQuestionBtn;
+    private String questionStr,optionAStr,optionBStr,optionCStr,optionDStr,answerStr;
+    private Dialog loadingDialog;
+    private FirebaseFirestore firestore;
+    private String action;
+    private int questionID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_detail);
+
+        Toolbar toolbar = findViewById(R.id.questionDetailToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        question = findViewById(R.id.question);
+        optionA = findViewById(R.id.optionA);
+        optionB = findViewById(R.id.optionB);
+        optionC = findViewById(R.id.optionC);
+        optionD = findViewById(R.id.optionD);
+        answer = findViewById(R.id.answer);
+        addQuestionBtn = findViewById(R.id.questionDetailBtn);
+
+        loadingDialog = new Dialog(QuestionDetailActivity.this);
+        loadingDialog.setContentView(R.layout.loading_progressbar);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progressbar_background);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        action = getIntent().getStringExtra("ACTION");
+
+        if(action.compareTo("EDIT") == 0)
+        {
+            questionID = getIntent().getIntExtra("Q_ID",0);
+            loadData(questionID);
+            getSupportActionBar().setTitle("Question" + String.valueOf(questionID + 1));
+            addQuestionBtn.setText("UPDATE");
+        }
+        else
+        {
+            getSupportActionBar().setTitle("Question" + String.valueOf(questionList.size() + 1));
+            addQuestionBtn.setText("ADD");
+        }
+
+        addQuestionBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v)
+            {
+                questionStr = question.getText().toString();
+                optionAStr = optionA.getText().toString();
+                optionBStr = optionB.getText().toString();
+                optionCStr = optionC.getText().toString();
+                optionDStr = optionD.getText().toString();
+                answerStr = answer.getText().toString();
+
+                if(questionStr.isEmpty())
+                {
+                    question.setError("Enter Question");
+                    return;
+                }
+                if(optionAStr.isEmpty())
+                {
+                    optionA.setError("Enter Option A");
+                    return;
+                }
+                if(optionBStr.isEmpty())
+                {
+                    optionB.setError("Enter Option B");
+                    return;
+                }
+                if(optionCStr.isEmpty())
+                {
+                    optionC.setError("Enter Option C");
+                    return;
+                }
+                if(optionDStr.isEmpty())
+                {
+                    optionD.setError("Enter Option D");
+                    return;
+                }
+                if(answerStr.isEmpty())
+                {
+                    answer.setError("Enter Answer");
+                    return;
+                }
+                if(action.compareTo("EDIT") == 0)
+                {
+                    editQuestion();
+                }
+                else
+                {
+                    addNewQuestion();
+                }
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void addNewQuestion()
+    {
+        loadingDialog.show();
+
+        Map<String,Object> questionData = new ArrayMap<>();
+
+        questionData.put("QUESTION",questionStr);
+        questionData.put("A",optionAStr);
+        questionData.put("B",optionBStr);
+        questionData.put("C",optionCStr);
+        questionData.put("D",optionDStr);
+        questionData.put("ANSWER",answerStr);
+
+        final String docID = firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                .collection(setIDs.get(selectedSetIndex)).document().getId();
+
+        firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                .collection(setIDs.get(selectedSetIndex)).document(docID).set(questionData)
+                .addOnSuccessListener(new OnSuccessListener<Void>()
+                {
+                    @Override
+                    public void onSuccess(Void aVoid)
+                    {
+                        Map<String,Object> questionDoc = new ArrayMap<>();
+                        questionDoc.put("Q" + String.valueOf(questionList.size() + 1) + "_ID",docID);
+                        questionDoc.put("COUNT",String.valueOf(questionList.size() + 1));
+
+                        firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                                .collection(setIDs.get(selectedSetIndex)).document("QUESTION_LIST").update(questionDoc)
+                                .addOnSuccessListener(new OnSuccessListener<Void>()
+                                {
+                                    @Override
+                                    public void onSuccess(Void aVoid)
+                                    {
+                                        Toast.makeText(QuestionDetailActivity.this,"Question Added Successfully",Toast.LENGTH_SHORT).show();
+
+                                        questionList.add(new QuestionModel(docID,questionStr,optionAStr,optionBStr,optionCStr,optionDStr,Integer.valueOf(answerStr)));
+
+                                        loadingDialog.dismiss();
+                                        QuestionDetailActivity.this.finish();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener()
+                                {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e)
+                                    {
+                                        Toast.makeText(QuestionDetailActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                                        loadingDialog.dismiss();
+                                    }
+                                });
+                    }
+                }).addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                Toast.makeText(QuestionDetailActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                loadingDialog.dismiss();
+            }
+        });
+
+    }
+    private void loadData(int id)
+    {
+        question.setText(questionList.get(id).getQuestion());
+        optionA.setText(questionList.get(id).getOptionA());
+        optionB.setText(questionList.get(id).getOptionB());
+        optionC.setText(questionList.get(id).getOptionC());
+        optionD.setText(questionList.get(id).getOptionD());
+        answer.setText(String.valueOf(questionList.get(id).getCorrectAnswer()));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void editQuestion()
+    {
+        loadingDialog.show();
+
+        Map<String,Object> questionData = new ArrayMap<>();
+        questionData.put("QUESTION",questionStr);
+        questionData.put("A",optionAStr);
+        questionData.put("B",optionBStr);
+        questionData.put("C",optionCStr);
+        questionData.put("D",optionDStr);
+        questionData.put("ANSWER",answerStr);
+
+        firestore.collection("QUIZ").document(catList.get(selectedCatIndex).getId())
+                .collection(setIDs.get(selectedSetIndex)).document(questionList.get(questionID).getQuestionID())
+                .set(questionData)
+                .addOnSuccessListener(new OnSuccessListener<Void>()
+                {
+                    @Override
+                    public void onSuccess(Void aVoid)
+                    {
+                        Toast.makeText(QuestionDetailActivity.this,"Question updated successfully",Toast.LENGTH_SHORT).show();
+                        questionList.get(questionID).setQuestion(questionStr);
+                        questionList.get(questionID).setOptionA(optionAStr);
+                        questionList.get(questionID).setOptionB(optionBStr);
+                        questionList.get(questionID).setOptionC(optionCStr);
+                        questionList.get(questionID).setOptionD(optionDStr);
+                        questionList.get(questionID).setCorrectAnswer(Integer.valueOf(answerStr));
+
+                        loadingDialog.dismiss();
+                        QuestionDetailActivity.this.finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener()
+                {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Toast.makeText(QuestionDetailActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        loadingDialog.dismiss();
+                    }
+                });
+
+    }
+}
