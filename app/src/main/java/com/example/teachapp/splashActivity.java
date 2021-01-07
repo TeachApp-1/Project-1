bfa559b16694e4c339008df769c52cf089fafb09
MikiVit <vimiki9@gmail.com>
2021-01-08 01:59:28 +0200

    Updated files

---
+++ app/src/main/java/com/example/teachapp/splashActivity.java
@@ -0,0 +1,67 @@
+package com.example.teachapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class splashActivity extends AppCompatActivity {
+
+    private TextView appName;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName =  findViewById(R.id.app_name);
+
+        Typeface typeface = ResourcesCompat.getFont(this,R.font.blacklist);
+        appName.setTypeface(typeface);
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.myanim);
+        appName.setAnimation(anim);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        DbQuery.g_firestore = FirebaseFirestore.getInstance();
+
+        new Thread() {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (mAuth.getCurrentUser() != null)
+                {
+                    Intent intent = new Intent(splashActivity.this, SelectLevelActivity.class);
+                    startActivity(intent);
+                    splashActivity.this.finish();
+                }
+                else
+                {
+                    Intent intent = new Intent(splashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    splashActivity.this.finish();
+                }
+            }
+        }.start();
+    }
+}
